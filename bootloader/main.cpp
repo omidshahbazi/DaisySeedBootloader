17fbdcef02f967c674bd36a49cf9368c029600e5
CorvusPrudens <corvusprudens@gmail.com>
2021-09-01 12:02:22 -0600

    Moved fatfs to its own file

--- bootloader/main.cpp
+++ bootloader/main.cpp
@@ -1,83 +1,11 @@
-#include <string.h>
-#include "dfu.h"
+#include "bootloader.h"
+#include "fatloader.h"
 #include "daisy_seed.h"
 
 using namespace daisy;
 
 DaisySeed hw;
-DFUHandle dfu;
-SdmmcHandler sd;
-
-enum Result {
-	ABSENT,
-	PRESENT,
-	ERR,
-};
-
-#define MAX_FILENAME_LEN 256
-#define TRY(func) if (func != FR_OK) return Result::PRESENT
-
-char binfile[MAX_FILENAME_LEN];
-
-Result LoadFAT(FILINFO* info)
-{
-	if (f_open(&SDFile, info->fname, FA_OPEN_EXISTING | FA_READ) != FR_OK)
-	{
-		return Result::ERR;
-	}
-	// right now, we're just expecting the raw binary
-	
-}
-
-Result CheckFAT()
-{
-	// Init SD Card
-	SdmmcHandler::Config sd_cfg;
-	sd_cfg.Defaults();
-	sd.Init(sd_cfg);
-
-	// Links libdaisy i/o to fatfs driver.
-	dsy_fatfs_init();
-
-	// Mount SD Card
-	if (f_mount(&SDFatFS, SDPath, 1) == FR_OK)
-	{
-		DIR dir;
-		FILINFO info;
-		FRESULT result = FR_OK;
-		char *  name;
-
-		if(f_opendir(&dir, SDPath) != FR_OK)
-    {
-        return;
-    }
-    do
-    {
-        result = f_readdir(&dir, &info);
-        // Exit if bad read or NULL fname
-        if(result != FR_OK || info.fname[0] == 0)
-            break;
-        // Skip if its a directory or a hidden file.
-        if(info.fattrib & (AM_HID | AM_DIR))
-            continue;
-
-        name = info.fname;
-
-				if(strstr(name, ".bin") || strstr(name, ".BIN"))
-				{
-					// strcpy(binfile, name);
-					LoadFAT(&info);
-				}
-    } while(result == FR_OK);
-
-		// Not sure if this needs to be closed before reading files
-    f_closedir(&dir);
-
-
-	}
-
-	return Result::ABSENT;
-}
+Bootloader boot;
 
 
 
@@ -98,9 +26,9 @@ int main(void)
 	// TODO -- usb drive
 
 	// Otherwise, wait for a DFU interaction
-	dfu.Init(&hw);
+	boot.dfu.Init(&hw);
 
 	while(1) {
-		dfu.PollJump();
+		boot.AwaitDFU();
 	}
 }
